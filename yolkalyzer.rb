b22f7456f5ea89dd70cbdf409022447b78eb8aa2
Joseph (YOLK) Chiocchi <input.output@gmail.com>
2012-04-12 01:28:05 -0500

    maybe works now

--- yolkalyzer.rb
+++ yolkalyzer.rb
@@ -1,5 +1,14 @@
 require 'xmlsimple'
 
-config = XmlSimple.xml_in('model.dae')
+data = XmlSimple.xml_in('model.dae')
 
-puts config
+data['library_geometries'].each do |ele|
+    ele.each do |k, v|
+        print v
+        print "\n"
+        print "="*20
+    end
+    print ele
+    print "\n\n\n"
+    print "-"*10
+end
