5eb727ffbb3cc878f72a559cbfefb75e530d8064
Joseph (YOLK) Chiocchi <input.output@gmail.com>
2012-05-10 05:31:53 -0500

    working order #2

--- yolkalyzer.rb
+++ yolkalyzer.rb
@@ -1,16 +1,27 @@
 require 'ap'
 require "rexml/document"
-doc = REXML::Document.new('model.dae')
+file = File.new( "Fauteil_new_coussin.dae" )
+doc = REXML::Document.new file
+newdoc = File.new("fuckedmodel2.dae", "w")
 
 class Array
   def shuffle!
     size.downto(1) { |n| push delete_at(rand(n)) }
     self
   end
+  def plusone!
+      size.downto(1){|n| n+=1.4 }
+      self
+  end
+end
+REXML::XPath.each( doc, "//p") do |element| 
+    elements = element.text.split(' ')
+    new_elements = []
+    elements.each{ |ele| new_elements << (Integer rand(Float ele)).to_s }
+    elements.replace(new_elements)
+    element.text = elements.join(' ')
 end
 
 
+doc.write newdoc
 
-doc.elements.each('p') do |ele|
-    ap ele
-end
