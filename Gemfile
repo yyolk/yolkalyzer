4f57f89cd1265491fcf3432db189ab47d211c0c3
Joseph (YOLK) Chiocchi <input.output@gmail.com>
2012-05-10 03:37:47 -0500

    before branch to rexml

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "http://rubygems.org"
 gem "xml-simple"
 gem "libxml-ruby"
+gem "awesome_print"
--- yolkalyzer.rb
+++ yolkalyzer.rb
@@ -1,14 +1,31 @@
 require 'xmlsimple'
+require 'ap'
 
-data = XmlSimple.xml_in('model.dae')
-
-data['library_geometries'].each do |ele|
-    ele.each do |k, v|
-        print v
-        print "\n"
-        print "="*20
-    end
-    print ele
-    print "\n\n\n"
-    print "-"*10
+xs = XmlSimple.new()
+data = xs.xml_in('model.dae')
+class Array
+  def shuffle!
+    size.downto(1) { |n| push delete_at(rand(n)) }
+    self
+  end
 end
+count = 0
+new_data = ['libary_geometries']
+data['].each do |ele|
+    #ele.each do |k, v| 
+    #    k.ea
+    #    print "count = "+count.to_s
+    #    count+=1
+    #    new_data.push({k=>v})
+    #    
+    #end
+    ap ele
+    
+end
+#
+#new_data.each do |ele|
+#    puts ele
+#    
+#end
+
+
