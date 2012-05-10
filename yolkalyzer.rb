3d41511707b9e495ae63a0704a797107826e89c3
Joseph (YOLK) Chiocchi <input.output@gmail.com>
2012-05-10 05:32:40 -0500

    working order #2

--- yolkalyzer.rb
+++ yolkalyzer.rb
@@ -2,7 +2,7 @@ require 'ap'
 require "rexml/document"
 file = File.new( "Fauteil_new_coussin.dae" )
 doc = REXML::Document.new file
-newdoc = File.new("fuckedmodel2.dae", "w")
+newdoc = File.new("fuckedmodel3.dae", "w")
 
 class Array
   def shuffle!
@@ -17,8 +17,9 @@ end
 REXML::XPath.each( doc, "//p") do |element| 
     elements = element.text.split(' ')
     new_elements = []
-    elements.each{ |ele| new_elements << (Integer rand(Float ele)).to_s }
-    elements.replace(new_elements)
+    #elements.each{ |ele| new_elements << (Integer rand(Float ele)).to_s }
+    elements.each{ |ele| new_elements << ele  }
+    elements.replace(new_elements.shuffle!)
     element.text = elements.join(' ')
 end
 
